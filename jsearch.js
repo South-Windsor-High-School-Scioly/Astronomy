660c262c39abcfbd12451c1f4a0fafab78e75d66
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-26 21:26:27 -0500

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -3,7 +3,13 @@ var query = "";
 
 function search(ele) {
     if(event.key === 'Enter') {
-        query = ele.value;   
+        query = ele.value;
+        if(query.substr(0,1)=="#"){
+          query = query.substr(1);
+        }
         //alert(query);
+    }
+    else if(event.key=='#'){
+      document.getElementById("w").value="";
     }
 }
