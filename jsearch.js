7c6ffc1a1a26e1412e4d57433fe3134b69fb8173
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-27 12:53:00 -0500

    Update jsearch.js

--- jsearch.js
+++ jsearch.js
@@ -1,7 +1,7 @@
 /*
 Some quick tricks:
-# - clears the search box (the program automatically removes # from beginning of queries if present)
-@ - redirects you to the other page
+2 - clears the search box (the program automatically removes # from beginning of queries if present)
+3 - redirects you to the other page
 Enter - searches for the given query
 */
 
@@ -10,15 +10,15 @@ var query = "";
 function search(ele) {
     if(event.key === 'Enter') {
         query = ele.value;
-        if(query.substr(0,1)=="#"){
+        if(query.substr(0,1)=="3"){
           query = query.substr(1);
         }
         //alert(query);
     }
-    else if(event.key=='#'){
+    else if(event.key=='3'){
       document.getElementById("w").value="";
     }
-    else if (event.key=='@'){
+    else if (event.key=='2'){
       if(document.title=="Power Search"){
         window.location.replace("pages.html");
       }
