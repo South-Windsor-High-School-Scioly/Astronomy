ae9ad2622eba479f4c5d2429bfeaab517e727c00
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-28 11:00:44 -0500

    e

--- jsearch.js
+++ jsearch.js
@@ -33,6 +33,13 @@ function search(ele) {
 
 import * as pdfjsDist from 'https://esm.run/pdfjs-dist';
 
-pdfjsLib.getDocument('helloworld.pdf');
+pdfjsLib.getDocument("Sources/Astro Notes-Overview.pdf");
+var loadingTask = pdfjsLib.getDocument('Sources/Astro Notes-Overview.pdf');
+loadingTask.promise.then(function(pdf) {
+  // you can now use *pdf* here
+});
+
+document.getElementById('canvas').innerHTML = "<p>luh</p>";
+
 
 
