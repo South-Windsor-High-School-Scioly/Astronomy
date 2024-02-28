3c4bcc86b349319ae64c977616e2c4e7cf32bb9a
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-28 12:46:32 -0500

    cluh

--- pdfjsp.js
+++ pdfjsp.js
@@ -1,4 +1,8 @@
 // PDF.js code below
 // See https://www.npmjs.com/package/pdfjs-dist documentation.
 
-import * as pdfjsDist from 'https://esm.run/pdfjs-dist';
+document.getElementsByClassName("canvas")[0].innerHTML = "<embed src='Sources/Astro Notes-Overview.pdf' width='80%' height='720px' style='margin-right: 10vw; margin-top:20px; float:right'/>";
+
+const pdfList = []; // File directory of every PDF object we have. 
+
+
