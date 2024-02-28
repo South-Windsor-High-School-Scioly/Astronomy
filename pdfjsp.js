8f48ef5638a8e36f24d154a7ab59253a419e19ae
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-28 14:07:41 -0500

    Update pdfjsp.js

--- pdfjsp.js
+++ pdfjsp.js
@@ -1,4 +1,6 @@
 // PDF.js code below
 // See https://www.npmjs.com/package/pdfjs-dist documentation.
 
+
+document.getElementsByClassName("canvas")[0].innerHTML = "<p>wsg luh</p>";
 console.log("luh");
