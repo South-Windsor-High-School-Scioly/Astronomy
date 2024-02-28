71a356ef2015653e91ab7df583e9f2c7b97a6f4b
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-28 14:04:44 -0500

    e

--- pdfjsp.js
+++ pdfjsp.js
@@ -1,8 +1,4 @@
 // PDF.js code below
 // See https://www.npmjs.com/package/pdfjs-dist documentation.
-import pdfjsDist from '../Astronomy/Pdfjs/build/pdf.js';
-
-pdfjsDist.getDocument("https://icseindia.org/document/sample.pdf");
 
 console.log("luh");
-
