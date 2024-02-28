a4a64ab5ec86182f1197b43c67ca3fe338ed4668
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-28 10:56:41 -0500

    luh

--- jsearch.js
+++ jsearch.js
@@ -28,3 +28,11 @@ function search(ele) {
     }
 }
 
+// PDF.js code below
+// See https://www.npmjs.com/package/pdfjs-dist documentation.
+
+import * as pdfjsDist from 'https://esm.run/pdfjs-dist';
+
+pdfjsLib.getDocument('helloworld.pdf');
+
+
