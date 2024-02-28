658147f5d0be20562a9ce03bff006c6c1cc5cfeb
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-28 16:51:27 -0500

    blud idek what happened

--- pdfjsp.js
+++ pdfjsp.js
@@ -1,7 +1,17 @@
-// PDF.js code below
-//document.getElementsByClassName("canvas")[0].innerHTML = "<p>wsg luh</p>";
-console.log("luh");
+//import * as pdfjs from "https://cdn.jsdelivr.net/npm/pdfjs-dist@4.0.379/+esm";
 
-const pdfjs = require("pdfjs-dist");
+const pdfjs = require("pdf.js");
+async function getContent(src){
+    const doc=await pdfjs.getDocument(src).promise;
+    const page = await doc.getPage(1);
+    return await page.getTextContent();
+}
 
+async function getItems(src){
+    const content = await getContent(src);
+    const items = content.items.map((item));
+    console.log(item.str);
+    return items;
+}
 
+getItems("./Sources/Astro Notes-Overview.pdf");
