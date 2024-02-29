c46f59167fae4f7728d1977855180f551fe48761
Aditya Jha <156379648+adityajha25@users.noreply.github.com>
2024-02-29 14:24:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -133,7 +133,7 @@ function dropbox(){
   for(let i=0;i<list.length;i++){
     const path = "PDF Sources/"+list[i].substring(0,list[i].length-4)+".pdf";
    // console.log(path);
-   document.getElementsByClassName("jawn")[0].innerHTML+="<embed style='padding-top:40px; width: 33%; min-height:100px' src=\'" + path+"\'>" + "" +"<div>"+path+"</div>";
+   document.getElementsByClassName("jawn")[0].innerHTML+="<embed style='padding-top:40px; width: 25%; min-height:100px' src=\'" + path+"\'>" + "" +"<div>"+path+"</div>";
   }
 }
 
